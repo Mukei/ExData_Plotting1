28c75db6f43cb5e45edfda440397c6065e39fa1f
Telandro Thomas <fillspam@gmail.com>
2014-08-10 02:57:47 +0900

    - Faster method to read data. (Method found on coursera forum → combines grep/pipes). - Removed use of data.table as manipulated data size is smaller now.

--- plot1.R
+++ plot1.R
@@ -5,29 +5,41 @@
 # following plots below, all of which were constructed using the base plotting system.
 
 ## plot1 ------------------------------------------------------------------
-# Load needed library -----------------------------------------------------
 
-# Working with data.table is computationally more effecient than working with data.frame
-library("data.table")
+fileName = "./exdata-data-household_power_consumption.txt"
 
-# Download the data if not already available in script folder -----------
-if(!file.exists("./exdata-data-household_power_consumption.zip"))
+# Check file availability -------------------------------------------------
+# Check if txt file available, check for zip file.
+# If no zip file to unzip, download the fileDownload the data if not already available in script folder
+if(!file.exists(fileName))
 {
-  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-  download.file(fileUrl, destfile = "./exdata-data-household_power_consumption.zip", method = "curl")
+  if(!file.exists("./exdata-data-household_power_consumption.zip"))
+  {
+    print("Downloading the data")
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileUrl, destfile = "./exdata-data-household_power_consumption.zip", method = "curl")
+  }
+  else
+  {
+    unzip("./exdata-data-household_power_consumption.zip")
+  }
 }
+
 
 # Read the data and put it into a data.frame ------------------------------
 print("Loading data")
-dt <- data.table(read.csv(unzip("./exdata-data-household_power_consumption.zip"), header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE))
 
-dt$dateValid <- as.Date(dt$Date, "%d/%m/%Y")
-dt <- dt[(dt$dateValid ==  "2007-02-01" | dt$dateValid ==  "2007-02-02"),]
-datetime <- strptime(paste(dt$Date, dt$Time), "%d/%m/%Y %H:%M:%S")
+# Below method shared on the Forum topics https://class.coursera.org/exdata-004/forum/thread?thread_id=19
+df <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), header = F, sep = ';', na.strings = "?", stringsAsFactors = FALSE, comment.char = "", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric", "numeric"))
+
+colnames(df) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+
+df$dateValid <- as.Date(df$Date, "%d/%m/%Y")
+datetime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
 
 
 # Figure 1 ----------------------------------------------------------------
 print("Plotting data & saving plot1.png (480x480px)")
 png(filename = "./plot1.png",width = 480, height = 480, units = "px")
-with(dt, hist(Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red"))
+with(df, hist(Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red"))
 dev.off()
--- plot4.R
+++ plot4.R
@@ -5,45 +5,59 @@
 # following plots below, all of which were constructed using the base plotting system.
 
 ## plot4 ------------------------------------------------------------------
-# Load needed library -----------------------------------------------------
 
-# Working with data.table is computationally more effecient than working with data.frame
-library("data.table")
+fileName = "./exdata-data-household_power_consumption.txt"
 
-# Download the data if not already available in script folder -----------
-if(!file.exists("./exdata-data-household_power_consumption.zip"))
+
+# Check file availability -------------------------------------------------
+# Check if txt file available, check for zip file.
+# If no zip file to unzip, download the fileDownload the data if not already available in script folder
+if(!file.exists(fileName))
 {
-  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-  download.file(fileUrl, destfile = "./exdata-data-household_power_consumption.zip", method = "curl")
+  if(!file.exists("./exdata-data-household_power_consumption.zip"))
+  {
+    print("Downloading the data")
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileUrl, destfile = "./exdata-data-household_power_consumption.zip", method = "curl")
+  }
+  else
+  {
+    unzip("./exdata-data-household_power_consumption.zip")
+  }
 }
 
 # Read the data and put it into a data.frame ------------------------------
-dt <- data.table(read.csv(unzip("./exdata-data-household_power_consumption.zip"), header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE))
+print("Loading data")
 
-dt$dateValid <- as.Date(dt$Date, "%d/%m/%Y")
-dt <- dt[(dt$dateValid ==  "2007-02-01" | dt$dateValid ==  "2007-02-02"),]
-datetime <- strptime(paste(dt$Date, dt$Time), "%d/%m/%Y %H:%M:%S")
+# Below method shared on the Forum topics https://class.coursera.org/exdata-004/forum/thread?thread_id=19
+df <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), header = F, sep = ';', na.strings = "?", stringsAsFactors = FALSE, comment.char = "", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric", "numeric"))
+
+colnames(df) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+
+df$dateValid <- as.Date(df$Date, "%d/%m/%Y")
+datetime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
 
 
 # Figure 4 ----------------------------------------------------------------
+print("Plotting data & saving plot3.png (480x480px)")
 png(filename = "./plot4.png",width = 480, height = 480, units = "px")
 # Create a 2x2 image plot
 par(mfrow = c(2,2))
 
 # Top left figure
-with(dt, plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
+with(df, plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
 
 # Top right figure
-with(dt, plot(datetime, Voltage, type = "l", ylab = "Voltage"))
+with(df, plot(datetime, Voltage, type = "l", ylab = "Voltage"))
 
 # Bottom left figure
-with(dt, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
-with(dt, lines(datetime, Sub_metering_2, type = "l", col = "red"))
-with(dt, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
+with(df, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+with(df, lines(datetime, Sub_metering_2, type = "l", col = "red"))
+with(df, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
 legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = par("lwd"), bty = "n")
 
 # Bottom right figure
-with(dt, plot(datetime, Global_reactive_power, type = "l"))
+with(df, plot(datetime, Global_reactive_power, type = "l"))
 
 # Save the plot in 480x480pixels png
 dev.off()
